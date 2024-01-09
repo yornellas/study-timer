71c7126074733cb4d0063285008f56eda4e01616
yornellas <yo_ornellas@hotmail.com>
2024-01-09 10:30:52 -0300

    feat: create list component

---
+++ src/components/list/index.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function List() {
+  return (
+    <aside>
+      <h2>Estudos do dia</h2>
+      <ul>
+        <li>
+          <h3>Tarefa 1</h3>
+          <span>tempo 1</span>
+        </li>
+        <li>
+          <h3>Tarefa 2</h3>
+          <span>tempo 2</span>
+        </li>
+      </ul>
+    </aside>
+  )
+}
+
+export default List
