82fffd7848accaba90c5249b681cc0f051e7e6d5
yornellas <yo_ornellas@hotmail.com>
2024-01-12 08:00:49 -0300

    feat: add clock component

---
+++ src/components/timer/clock/index.tsx
@@ -0,0 +1,13 @@
+import './styles.scss'
+
+export function Clock() {
+  return (
+    <>
+      <span className="number">0</span>
+      <span className="number">0</span>
+      <span>:</span>
+      <span className="number">0</span>
+      <span className="number">0</span>
+    </>
+  )
+}
