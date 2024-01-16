8bba0cb225a8c04b6bdce95a319496e85369fa7e
yornellas <yo_ornellas@hotmail.com>
2024-01-16 08:03:06 -0300

    feat: implements state on Form class component

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,9 +3,18 @@ import Button from '../button'
 import './styles.scss'
 
 class Form extends React.Component {
+  state = {
+    task: '',
+    time: '00:00'
+  }
+
+  addTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <form className='newTask'>
+      <form className='newTask' onSubmit={ this.addTask.bind(this) }>
         <div className='inputContainer'>
           <label htmlFor='task'>Adicione um novo estudo</label>
           <input
@@ -14,6 +23,12 @@ class Form extends React.Component {
             id='task'
             placeholder='O que você quer estudar?'
             required
+            value={ this.state.task }
+            onChange={(event) =>
+              this.setState(
+                { ...this.state, task: event.target.value }
+              )
+            }
             />
         </div>
         <div className='inputContainer'>
@@ -25,6 +40,11 @@ class Form extends React.Component {
             id='time'
             min='00:00:00'
             max='02:00:00'
+            value={ this.state.time }
+            onChange={(event) => 
+              this.setState(
+                { ...this.state, time: event.target.value }
+            )}
           />
         </div>
         <Button 
